017cbcc28f7b17ab1433b9b41ec7b0cd6ede2d30
Michel dos Santos <michelsantos15@gmail.com>
2020-07-29 23:46:45 -0300

    Initial commit

---
+++ app/src/main/java/micheldev/com/motivation/SplashActivity.kt
@@ -0,0 +1,13 @@
+package micheldev.com.motivation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        if(supportActionBar != null) supportActionBar!!.hide()
+    }
+}
